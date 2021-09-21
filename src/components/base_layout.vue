48c2d3409b682359b958acba2f129d0c7bbae983
shasless <Efrei2018>
2021-09-21 09:44:50 +0200

    Ex 7: create BaseButton with primary color

--- src/components/base_layout.vue
+++ src/components/base_layout.vue
@@ -1,5 +1,14 @@
 <template><div>
   <base_header />
+  <div>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <b-button variant="primary">Button</b-button>
+    <b-button disabled variant="primary">base buton disabl</b-button>
+
+  </div>
   <base_footer />
 </div>
 
--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faFontAwesome } from '@fortawesome/free-brands-svg-icons'
+import {BootstrapVue} from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
 
 library.add(faUserSecret)
@@ -11,6 +14,8 @@ library.add(faFontAwesome)
 
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.use(BootstrapVue);
+
 
 
 Vue.config.productionTip = false
